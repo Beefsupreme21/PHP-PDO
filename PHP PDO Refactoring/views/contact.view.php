20422a51fff9d6323dddfd07467bb88990bc6cb6
Cory S <101227906+Beefsupreme21@users.noreply.github.com>
2022-07-18 22:57:30 -0600

    Add files via upload

--- PHP PDO Refactoring/views/contact.view.php
+++ PHP PDO Refactoring/views/contact.view.php
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html lang='en'>
-<head>
-    <meta charset='UTF-8'>
-    <title>Document</title>
-</head>
-<body>
-    <h1>Contact</h1>
-    
-</body>
+<?php require('partials/head.php'); ?>
 
-</html>
+<h1>Contact</h1>
+    
+<?php require('partials/footer.php'); ?>
--- PHP PDO Refactoring/views/index.view.php
+++ PHP PDO Refactoring/views/index.view.php
@@ -1,19 +1,15 @@
-<!DOCTYPE html>
- 
- <html>
-    <body>
-        <ul>    
-            <?php foreach ($tasks as $task) : ?>
-                <li>
-                    <?php if ($task->completed) : ?>
-                        <strike><?= $task->description; ?></strike>
-                    <?php else: ?>
-                        <?= $task->description; ?>
-                    <?php endif; ?>
-                </li>
-            <?php endforeach; ?>
-        </ul>
-    </body>
- </html> 
- 
+<?php require('partials/head.php'); ?>
+
+<?php foreach ($tasks as $task) : ?>
+    <li>
+        <?php if ($task->completed) : ?>
+            <strike><?= $task->description; ?></strike>
+        <?php else: ?>
+            <?= $task->description; ?>
+        <?php endif; ?>
+    </li>
+<?php endforeach; ?>
+
+<?php require('partials/footer.php'); ?>
+
  
